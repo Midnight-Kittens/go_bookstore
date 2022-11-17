a0e4a3c5da06eb6426052de6d39ac2fbaba64c67
Midnight-Kittens <105041614+Midnight-Kittens@users.noreply.github.com>
2022-11-17 09:13:07 -0800

    Update app.go

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "Midnight-Kittens:Wagontrain@22/simplerest?charset=utf8&parseTime=True&Loc=Local")
+	d, err := gorm.Open("mysql", "Midnight-Kittens:**************/simplerest?charset=utf8&parseTime=True&Loc=Local")
 	if err != nil {
 		panic(err)
 	}
